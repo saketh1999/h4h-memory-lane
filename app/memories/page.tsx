77bf6f8aa33f6ef75f38f25759d17b870441eb9f
ellajackson8377 <ellajackson8377@gmail.com>
2025-02-15 14:02:06 -0800

    Update page.tsx

--- app/memories/page.tsx
+++ app/memories/page.tsx
@@ -5,11 +5,11 @@ import Masonry from "@/components/Mansory";
 
 export default function MemoriesPage() {
   const data = [
-    { id: 1, image: 'https://picsum.photos/id/10/200/300', height: 400 },
-    { id: 2, image: 'https://picsum.photos/id/14/200/300', height: 300 },
-    { id: 3, image: 'https://picsum.photos/id/15/200/300', height: 300 },
-    { id: 4, image: 'https://picsum.photos/id/16/200/300', height: 300 },
-    { id: 5, image: 'https://picsum.photos/id/17/200/300', height: 300 },
+    { id: 1, image: 'images/IMG_8191.jpg', height: 700 },
+    { id: 2, image: 'images/IMG_8192.jpg', height: 600 },
+    { id: 3, image: 'images/IMG_8193.jpg', height: 600 },
+    { id: 4, image: 'images/IMG_8194.jpg', height: 600 },
+    { id: 5, image: 'images/IMG_8194.jpg', height: 600 },
     { id: 6, image: 'https://picsum.photos/id/19/200/300', height: 300 },
     { id: 7, image: 'https://picsum.photos/id/37/200/300', height: 200 },
     { id: 8, image: 'https://picsum.photos/id/39/200/300', height: 300 },
